595634d429efa5dfae0acccb918572bf643cd112
cheeser <>
2004-08-02 16:30:54 +0000

    branch:      javabot fixed NPE in NickOMeter.  update sample config

--- src/javabot/operations/NickometerOperation.java
+++ src/javabot/operations/NickometerOperation.java
@@ -2,12 +2,10 @@ package javabot.operations;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javabot.BotEvent;
-import javabot.Message;
-
 import com.rickyclarkson.java.util.Arrays;
 import com.rickyclarkson.java.util.TypeSafeList;
+import javabot.BotEvent;
+import javabot.Message;
 
 /**
  * @author ricky_clarkson
@@ -18,40 +16,32 @@ public class NickometerOperation implements BotOperation {
      */
     public List handleMessage(BotEvent event) {
         List messages = new TypeSafeList(new ArrayList(), Message.class);
-
         String message = event.getMessage();
-
         Object[] messageParts = message.split(" ");
-
         messageParts = Arrays.removeAll(messageParts, "");
-
-        if (!messageParts[0].equals("nickometer") || messageParts.length < 2)
+        if(messageParts == null
+            || !messageParts[0].equals("nickometer")
+            || messageParts.length < 2) {
             return messages;
-
+        }
         String nick = messageParts[1].toString();
-
-        if (nick.length() == 0)
+        if(nick.length() == 0) {
             return messages;
-
+        }
         int lameness = 0;
-
-        if (nick.length() > 0 && Character.isUpperCase(nick.charAt(0)))
+        if(nick.length() > 0 && Character.isUpperCase(nick.charAt(0))) {
             lameness--;
-
-        for (int a = 0; a < nick.length(); a++)
-            if (!Character.isLowerCase(nick.charAt(a)))
+        }
+        for(int a = 0; a < nick.length(); a++) {
+            if(!Character.isLowerCase(nick.charAt(a))) {
                 lameness++;
-
+            }
+        }
         double tempLameness = (double)lameness / nick.length();
-
         tempLameness = Math.sqrt(tempLameness);
-
         lameness = (int)(tempLameness * 100);
-
         messages.add(new Message(event.getChannel(), "The nick " + nick
             + " is " + lameness + "% lame.", false));
-
         return messages;
     }
-
 }
