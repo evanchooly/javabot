ed8d85be9413ecb67b626b188967e746fb56cd16
cheeser <>
2005-07-20 18:57:44 +0000

    branch:      javabot new script

---
+++ bin/changes.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+STARTDATE=`date -d "1 day ago" +"%Y-%m-%d 00:00:00"`
+ENDDATE=`date +"%Y-%m-%d 00:00:00"`
+echo `date` : Looking for new factoids between ${STARTDATE} and ${ENDDATE}
+psql -U javabot javabot -c "select username, name, value from factoids where name not like 'karma%' and updated between date('${STARTDATE}') and date('${ENDDATE}') order by name"
