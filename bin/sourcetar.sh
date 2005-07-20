a389cfbd530e83b0ded07d1535f25ff304b205e1
cheeser <>
2005-07-20 19:16:08 +0000

    branch:      javabot script updates

--- bin/sourcetar.sh
+++ bin/sourcetar.sh
@@ -9,4 +9,3 @@ cd /tmp
 /usr/bin/rm  ${TAR_DIR}/javabot-src-*.tar*
 /bin/tar --exclude=".svn" -cjvf ${TAR_DIR}/${TAR_NAME} javabot
 /usr/bin/rm -rf /tmp/javabot
-/usr/bin/echo "<html><body><a href=\"${TAR_NAME}\">${TAR_NAME}</a></body></html>" > ${TAR_DIR}/index.html
