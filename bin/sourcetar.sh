0ef564e4928efeb9d424c49d1de60ab45fb626b8
cheeser <>
2005-07-20 18:56:59 +0000

    branch:      javabot script updates

--- bin/sourcetar.sh
+++ bin/sourcetar.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 
 REVISION=$1
-cd /tmp
-svn co http://www.cheeseronline.org/svn/repos/javabot
-#REVISION="`svn info javabot | grep Revision: | cut -d: -f2 | cut -c2-`"
 TAR_DIR=/home/javabot/public_html/
-rm  ${TAR_DIR}/javabot-src-*.tar*
-tar --exclude=".svn" -cjvf ${TAR_DIR}/javabot-src-${REVISION}.tar.bz2 javabot
 
-#rm -r javabot
+cd /tmp
+/usr/bin/svn co http://www.cheeseronline.org/svn/repos/javabot
+/usr/bin/rm  ${TAR_DIR}/javabot-src-*.tar*
+/bin/tar --exclude=".svn" -cjvf ${TAR_DIR}/javabot-src-${REVISION}.tar.bz2 javabot
