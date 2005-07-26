9151526305d1e924d87736520a5a4e1e74846ae5
cheeser <>
2005-07-26 03:57:38 +0000

    branch:      javabot reversed the order for the log

--- bin/logchanges.sh
+++ bin/logchanges.sh
@@ -4,4 +4,4 @@ STARTDATE=`date -d "1 day ago" +"%Y-%m-%d 00:00:00"`
 STARTDATE=`date -d "1 week ago" +"%Y-%m-%d 00:00:00"`
 ENDDATE=`date -d "+1 day" +"%Y-%m-%d %H:%M:%S"`
 echo `date` : Looking for new factoids between ${STARTDATE} and ${ENDDATE}
-psql javabot -Ht -c "select message from changes where changeDate between date('${STARTDATE}') and date('${ENDDATE}') order by changeDate"
+psql javabot -Ht -c "select changeDate, message from changes where changeDate between date('${STARTDATE}') and date('${ENDDATE}') order by changeDate desc"
