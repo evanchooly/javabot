2ba8b8efabd54ed3a2da8958127f1e8b3eafb1c5
cheeser <>
2005-08-05 13:41:25 +0000

    branch:      javabot

--- bin/changes.sh
+++ bin/changes.sh
@@ -3,4 +3,4 @@
 STARTDATE=`date -d "1 day ago" +"%Y-%m-%d 00:00:00"`
 ENDDATE=`date +"%Y-%m-%d 00:00:00"`
 echo `date` : Looking for new factoids between ${STARTDATE} and ${ENDDATE}
-psql -U javabot javabot -c "select username, name, value from factoids where name not like 'karma%' and updated between date('${STARTDATE}') and date('${ENDDATE}') order by name"
+psql -U javabot javabot -c "select username, name, value from factoids where updated between date('${STARTDATE}') and date('${ENDDATE}') order by name"
--- bin/logchanges.sh
+++ bin/logchanges.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-STARTDATE=`date -d "1 day ago" +"%Y-%m-%d 00:00:00"`
 STARTDATE=`date -d "1 week ago" +"%Y-%m-%d 00:00:00"`
-ENDDATE=`date -d "+1 day" +"%Y-%m-%d %H:%M:%S"`
+STARTDATE=`date -d "1 week ago" +"%Y-%m-%d %H:%M:%S"`
+ENDDATE=`date +"%Y-%m-%d %H:%M:%S"`
 echo `date` : Looking for new factoids between ${STARTDATE} and ${ENDDATE}
 psql javabot -Ht -c "select changeDate, message from changes where changeDate between date('${STARTDATE}') and date('${ENDDATE}') order by changeDate desc"
