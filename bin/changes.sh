e5544eebf559e48273e6d6f255d3e4dae138fc2b
cheeser <>
2005-08-14 19:41:03 +0000

    branch:      javabot fixed date ranges

--- bin/changes.sh
+++ bin/changes.sh
@@ -2,4 +2,4 @@
 
 STARTDATE=`date -d "1 day ago" +"%Y-%m-%d 00:00:00"`
 echo `date` : Looking for new factoids between ${STARTDATE}
-psql -U javabot javabot -c "select username, name, value from factoids where updated > date('${STARTDATE}') order by name"
+psql -U javabot javabot -c "select username, name, value from factoids where updated > timestamp '${STARTDATE}' order by name"
--- bin/logchanges.sh
+++ bin/logchanges.sh
@@ -2,4 +2,4 @@
 
 STARTDATE=`date -d "1 week ago"`
 echo `date` : Looking for new factoids after ${STARTDATE}
-psql javabot -Ht -c "select changeDate, message from changes where changeDate > date('${STARTDATE}') order by changeDate desc"
+psql javabot -Ht -c "select changeDate, message from changes where changeDate > timestamp '${STARTDATE}' order by changeDate desc"
