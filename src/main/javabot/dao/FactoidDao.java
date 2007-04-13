3e223052ce3747a04f92157755fc670a7e475eb6
joed <>
2007-04-13 13:50:13 +0000

    branch:      javabot Complete DAO support for changes karma and factoids

--- src/main/javabot/dao/FactoidDao.java
+++ src/main/javabot/dao/FactoidDao.java
@@ -2,6 +2,9 @@ package javabot.dao;
 
 import javabot.dao.model.factoids;
 
+import java.util.List;
+
+import org.jdom.Element;
 
 // User: joed
 // Date: Apr 11, 2007
@@ -10,9 +13,19 @@ import javabot.dao.model.factoids;
 // 
 public interface FactoidDao {
 
+    boolean hasFactoid(String key);
 
-    public factoids load(String name);
+    void addFactoid(String sender, String key, String value, ChangesDao c_dao, String htmlFile);
 
+    void forgetFactoid(String sender, String key, ChangesDao c_dao, String htmlFile);
+
+    factoids getFactoid(String key);
+
+    Long getNumberOfFactoids();
+
+    List<factoids> getFactoids();
+
+    void updateFactoid(factoids factoid,ChangesDao c_dao, String htmlFile);
 
 
 }
