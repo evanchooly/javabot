40c4f7fc629461abe1d6d21336b41e483faf7a5c
joed <>
2007-05-21 17:52:35 +0000

    branch:      javabot Ajaxy factoid counter panel 30 seconds delay - cleanup

--- src/main/java/wicket/panels/JavabotInfo.java
+++ src/main/java/wicket/panels/JavabotInfo.java
@@ -1,11 +1,8 @@
 package wicket.panels;
 
-import javabot.dao.FactoidDao;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
 import org.apache.wicket.util.time.Duration;
 
 // Author: joed
@@ -13,14 +10,11 @@ import org.apache.wicket.util.time.Duration;
 // Date  : May 18, 2007
 public class JavabotInfo extends Panel {
 
-    @SpringBean
-    FactoidDao factoid;
-
     public JavabotInfo(String s) {
         super(s);
         add(new ExternalLink("homepage", "http://sourceforge.net/projects/fn-javabot", "HomePage"));
 
-        FactoidCount factoidCount = new FactoidCount("factoid_count"); 
+        FactoidCount factoidCount = new FactoidCount("factoid_count");
 
         add(factoidCount);
         factoidCount.add(new AjaxSelfUpdatingTimerBehavior(Duration.seconds(30)));
