98ab17d5355f1ff7a4d962e1d3355cc2d503c066
joed <>
2007-05-22 22:53:38 +0000

    branch:      javabot missing panels

--- Install-Manually.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mvn install:install-file -Dfile=lib/rickyclarkson.jar -DgroupId=rickyclarkson -DartifactId=rickyclarkson -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/jweather-0.2.5.jar -DgroupId=net.sf.jweather -DartifactId=jweather -Dversion=0.2.5 -Dpackaging=jar
-
--- src/main/java/javabot/ChannelControl.java
+++
@@ -1,7 +0,0 @@
-package javabot;
-
-public interface ChannelControl
-{
-	void partChannel(String channel,String leaveMessage);
-	void joinChannel(String channel);
-}
