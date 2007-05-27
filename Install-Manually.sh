d4335c0e2a3087fdb6c58d9805930417430dfbd1
joed <>
2007-05-27 00:00:14 +0000

    branch:      javabot Openlaszlo integrations for Admin interface Using Maven2's WAR dependency feature

--- Install-Manually.sh
+++ Install-Manually.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 mvn install:install-file -Dfile=lib/rickyclarkson.jar -DgroupId=rickyclarkson -DartifactId=rickyclarkson -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/jweather-0.2.5.jar -DgroupId=net.sf.jweather -DartifactId=jweather -Dversion=0.2.5 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/openlaszlo-4.0.2-servlet.war -DgroupId=com.openlaszlo -DartifactId=lps -Dversion=4.0.2 -Dpackaging=war
+
 
