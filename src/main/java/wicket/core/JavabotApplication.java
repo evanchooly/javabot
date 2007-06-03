f538d13d4f9c7e9dd88473f971d8240a45988c86
joed <>
2007-06-03 21:58:59 +0000

    branch:      javabot

--- src/main/java/wicket/core/JavabotApplication.java
+++ src/main/java/wicket/core/JavabotApplication.java
@@ -2,6 +2,7 @@ package wicket.core;
 
 
 import org.apache.wicket.Request;
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.Response;
 import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.AjaxServerAndClientTimeFilter;
@@ -9,7 +10,6 @@ import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import wicket.pages.Index;
 
-//
 // User: joed
 // Date: May 17, 2007
 // Time: 2:35:38 PM
@@ -22,13 +22,16 @@ public class JavabotApplication extends WebApplication {
     }
 
     protected void init() {
-	 addComponentInstantiationListener(new SpringComponentInjector(this));
+        addComponentInstantiationListener(new SpringComponentInjector(this));
 
         getResourceSettings().setThrowExceptionOnMissingResource(false);
         getRequestCycleSettings().addResponseFilter(new AjaxServerAndClientTimeFilter());
         getDebugSettings().setAjaxDebugModeEnabled(true);
-       // getRequestCycleSettings().setBufferResponse(false);
+        // getRequestCycleSettings().setBufferResponse(false);
 
+
+        mountBookmarkablePage("/home", Index.class);
+    
     }
 
     public Class getHomePage() {
