c01d3129a88d8af2a8339249de50869b2b44d9f6
joed <>
2007-07-10 06:01:06 +0000

    branch:      javabot Oki, this is almost as ugly as perl. But works.

--- src/main/java/javabot/operations/SpecialCasesOperation.java
+++ src/main/java/javabot/operations/SpecialCasesOperation.java
@@ -1,16 +1,20 @@
 package javabot.operations;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javabot.BotEvent;
 import javabot.Message;
 import javabot.Responder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author ricky_clarkson
  */
 public class SpecialCasesOperation implements BotOperation {
+
+    private static final Log log = LogFactory.getLog(SpecialCasesOperation.class);
     private final Responder responder;
 
     public SpecialCasesOperation(final Responder responder) {
@@ -24,15 +28,27 @@ public class SpecialCasesOperation implements BotOperation {
         List<Message> messages = new ArrayList<Message>();
         String message = event.getMessage();
         String lowerMessage = message.toLowerCase();
-        if(lowerMessage.startsWith("no, ") || lowerMessage.startsWith("no ")) {
-            message = message.substring("no, ".length());
+        if (lowerMessage.startsWith("no")) {
+            message = message.substring("no".length());
+
+            if (message.startsWith(",")) {
+                message = message.substring(",".length());
+            }
+
+            message = message.replaceAll("^\\s+", "");
+
+            log.debug("SpecialCasesOperation: " + message);
+
             String key = message.substring(0, message.indexOf(" is "));
+            key = key.replaceAll("^\\s+", "");
+
+            log.debug("SpecialCasesOperation: Key " + key);
             responder.getResponses(event.getChannel(), event.getSender(),
-                event.getLogin(), event.getHostname(), "forget " + key
+                    event.getLogin(), event.getHostname(), "forget " + key
             );
             messages.addAll(responder.getResponses(event.getChannel(),
-                event.getSender(), event.getLogin(), event.getHostname(),
-                message));
+                    event.getSender(), event.getLogin(), event.getHostname(),
+                    message));
             return messages;
         }
         return messages;
