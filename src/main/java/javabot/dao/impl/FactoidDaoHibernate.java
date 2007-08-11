22539cc50f1f843ff9792a73371eb3dba7a72673
joed <>
2007-08-11 18:23:31 +0000

    branch:      javabot For some reason we lost session here. I pickup the ID and make sure the EntityManager wakes up before we commit the data.

--- src/main/java/javabot/dao/impl/FactoidDaoHibernate.java
+++ src/main/java/javabot/dao/impl/FactoidDaoHibernate.java
@@ -1,15 +1,15 @@
 package javabot.dao.impl;
 
-import java.util.Date;
-import java.util.List;
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
-
 import javabot.dao.AbstractDaoHibernate;
 import javabot.dao.ChangeDao;
 import javabot.dao.FactoidDao;
 import javabot.dao.util.QueryParam;
 import javabot.model.Factoid;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import java.util.Date;
+import java.util.List;
 
 public class FactoidDaoHibernate extends AbstractDaoHibernate<Factoid> implements FactoidDao {
     private ChangeDao changeDao;
@@ -21,26 +21,27 @@ public class FactoidDaoHibernate extends AbstractDaoHibernate<Factoid> implement
     @SuppressWarnings("unchecked")
     public List<Factoid> find(QueryParam qp) {
         StringBuilder query = new StringBuilder("from Factoid f");
-        if(qp.hasSort()) {
+        if (qp.hasSort()) {
             query.append(" order by ")
-                .append(qp.getSort())
-                .append((qp.isSortAsc()) ? " asc" : " desc");
+                    .append(qp.getSort())
+                    .append((qp.isSortAsc()) ? " asc" : " desc");
         }
         return getEntityManager().createQuery(query.toString())
-            .setFirstResult(qp.getFirst())
-            .setMaxResults(qp.getCount()).getResultList();
+                .setFirstResult(qp.getFirst())
+                .setMaxResults(qp.getCount()).getResultList();
     }
 
     @SuppressWarnings({"unchecked"})
     public List<Factoid> getFactoids() {
-        return (List<Factoid>)getEntityManager().createNamedQuery(ALL).getResultList();
+        return (List<Factoid>) getEntityManager().createNamedQuery(ALL).getResultList();
     }
 
     public void save(Factoid factoid) {
         factoid.setUpdated(new Date());
+        super.getEntityManager().flush();
         super.save(factoid);
         changeDao
-            .logChange(factoid.getUserName() + " changed '" + factoid.getName() + "' to '" + factoid.getValue() + "'");
+                .logChange(factoid.getUserName() + " changed '" + factoid.getName() + "' to '" + factoid.getValue() + "'");
     }
 
     public boolean hasFactoid(String key) {
@@ -49,12 +50,14 @@ public class FactoidDaoHibernate extends AbstractDaoHibernate<Factoid> implement
 
     public void addFactoid(String sender, String key, String value) {
         Factoid factoid = new Factoid();
+        factoid.setId(factoid.getId());
         factoid.setName(key);
         factoid.setValue(value);
         factoid.setUserName(sender);
         factoid.setUpdated(new Date());
         super.save(factoid);
         changeDao.logAdd(sender, key, value);
+
     }
 
     public void delete(String sender, String key) {
@@ -66,22 +69,22 @@ public class FactoidDaoHibernate extends AbstractDaoHibernate<Factoid> implement
     public Factoid getFactoid(String name) {
         Factoid factoid = null;
         try {
-            factoid = (Factoid)getEntityManager().createNamedQuery(FactoidDao.BY_NAME)
-                .setParameter("name", name)
-                .getSingleResult();
-        } catch(NoResultException e) {
+            factoid = (Factoid) getEntityManager().createNamedQuery(FactoidDao.BY_NAME)
+                    .setParameter("name", name)
+                    .getSingleResult();
+        } catch (NoResultException e) {
         }
         return factoid;
     }
 
     public Long count() {
         String query = FactoidDao.COUNT;
-        return (Long)getEntityManager().createQuery("select count(*) from Factoid").getSingleResult();
+        return (Long) getEntityManager().createQuery("select count(*) from Factoid").getSingleResult();
 
     }
 
     public Long factoidCountFiltered(Factoid filter) {
-        return (Long)buildFindQuery(null, filter, true).getSingleResult();
+        return (Long) buildFindQuery(null, filter, true).getSingleResult();
     }
 
     @SuppressWarnings({"unchecked"})
@@ -91,34 +94,34 @@ public class FactoidDaoHibernate extends AbstractDaoHibernate<Factoid> implement
 
     private Query buildFindQuery(QueryParam qp, Factoid filter, boolean count) {
         StringBuilder hql = new StringBuilder();
-        if(count) {
+        if (count) {
             hql.append("select count(*) ");
         }
         hql.append(" from Factoid target where 1=1 ");
-        if(filter.getName() != null) {
+        if (filter.getName() != null) {
             hql.append("and upper(target.name) like :name ");
         }
-        if(filter.getUserName() != null) {
+        if (filter.getUserName() != null) {
             hql.append("and upper(target.userName) like :username ");
         }
-        if(filter.getValue() != null) {
+        if (filter.getValue() != null) {
             hql.append("and upper(target.value) like :value ");
         }
-        if(!count && qp != null && qp.hasSort()) {
+        if (!count && qp != null && qp.hasSort()) {
             hql.append("order by upper(target.").append(qp.getSort()).append(
-                ") ").append((qp.isSortAsc()) ? " asc" : " desc");
+                    ") ").append((qp.isSortAsc()) ? " asc" : " desc");
         }
         Query query = getEntityManager().createQuery(hql.toString());
-        if(filter.getName() != null) {
+        if (filter.getName() != null) {
             query.setParameter("name", "%" + filter.getName().toUpperCase() + "%");
         }
-        if(filter.getUserName() != null) {
+        if (filter.getUserName() != null) {
             query.setParameter("username", "%" + filter.getUserName().toUpperCase() + "%");
         }
-        if(filter.getValue() != null) {
+        if (filter.getValue() != null) {
             query.setParameter("value", "%" + filter.getValue().toUpperCase() + "%");
         }
-        if(!count && qp != null) {
+        if (!count && qp != null) {
             query.setFirstResult(qp.getFirst()).setMaxResults(qp.getCount());
         }
         return query;
