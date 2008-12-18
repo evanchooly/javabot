eddcc36bd69f5994b4cabe7cb57d4ca64b8f5e68
jlee@mandarb.local <>
2008-12-18 14:11:49 -0500

    branch:      javabot authenticate!

--- src/main/java/javabot/admin/Home.java
+++ src/main/java/javabot/admin/Home.java
@@ -1,10 +1,9 @@
 package javabot.admin;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 
-public class Home extends /*Authenticated*/WebPage {
+public class Home extends AuthenticatedWebPage {
     public Home(PageParameters parameters) {
         add(new FeedbackPanel("feedback"));
         add(new ChannelConfigPanel("channels"));
