d56e0e41cde91e71353638783ba3bd005abf8ecd
cheeser <>
2009-01-31 21:35:27 -0500

    branch:      javabot enhanced test

--- core/src/test/java/javabot/dao/LogDaoTest.java
+++ core/src/test/java/javabot/dao/LogDaoTest.java
@@ -36,7 +36,7 @@ public class LogDaoTest extends BaseServiceTest {
         cal.set(Calendar.MONTH, Calendar.JULY);
         cal.set(Calendar.YEAR, 2007);
         final Config config = dao.get();
-        config.setHistoryLength(1);
+        config.setHistoryLength(null);
         dao.save(config);
         final Calendar today = Calendar.getInstance();
         final SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
@@ -53,6 +53,11 @@ public class LogDaoTest extends BaseServiceTest {
         today.add(Calendar.MONTH, -1);
         today.add(Calendar.DAY_OF_YEAR, -1);
         Assert.assertFalse(logsDao.dailyLog(getJavabotChannel(), today.getTime()).isEmpty(), "History should be there");
+        logsDao.pruneHistory();
+        Assert.assertFalse(logsDao.dailyLog(getJavabotChannel(), today.getTime()).isEmpty(), "History should be there");
+        config.setHistoryLength(1);
+        dao.save(config);
+
         logsDao.pruneHistory();
         Assert.assertTrue(logsDao.dailyLog(getJavabotChannel(), today.getTime()).isEmpty(), "History should be gone");
     }
