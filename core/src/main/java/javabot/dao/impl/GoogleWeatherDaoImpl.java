71e53248702d157ec446834c1ccf203bf67815cd
evanchooly <jlee@antwerkz.com>
2011-09-14 09:22:40 -0400

    commit it

--- core/src/main/java/javabot/dao/impl/GoogleWeatherDaoImpl.java
+++ core/src/main/java/javabot/dao/impl/GoogleWeatherDaoImpl.java
@@ -1,14 +1,8 @@
 package javabot.dao.impl;
 
 import java.net.URL;
-
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-import javabot.model.Weather;
+
 import javabot.dao.WeatherDao;
 import javabot.model.Weather;
 import org.springframework.stereotype.Component;
@@ -26,14 +20,16 @@ public class GoogleWeatherDaoImpl implements WeatherDao {
     private static final String API_URL = "http://www.google.com/ig/api?weather=";
 
     public Weather getWeatherFor(final String place) {
+        GoogleWeatherSaxHandler handler = new GoogleWeatherSaxHandler();
         try {
             XMLReader reader = XMLReaderFactory.createXMLReader();
-            reader.setContentHandler(handler); 
+            reader.setContentHandler(handler);
             reader.parse(new InputSource(new URL(API_URL + URLEncoder.encode(place, "UTF-8"))
-                        .openStream()));
+                .openStream()));
         } catch (Exception e) {
             //TODO proper logging/distinction between error vs no-data
             return null;
         }
+        return handler.getWeather();
     }
 }
