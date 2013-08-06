fca9827b59b301fcdc9946b4643ec565e6122cae
evanchooly <jlee@antwerkz.com>
2013-08-05 22:45:05 -0400

    guard against NPE

--- core/src/main/java/javabot/commands/LockFactoid.java
+++ core/src/main/java/javabot/commands/LockFactoid.java
@@ -20,6 +20,7 @@ import javabot.model.Factoid;
 public class LockFactoid extends AdminCommand {
   @Param(primary = true)
   String name;
+
   @Inject
   private FactoidDao dao;
 
@@ -32,15 +33,19 @@ public class LockFactoid extends AdminCommand {
   public List<Message> execute(final Javabot bot, final IrcEvent event) {
     final List<Message> responses = new ArrayList<Message>();
     final String command = args.get(0);
-    final Factoid factoid = dao.getFactoid(name);
-    if ("lock".equals(command)) {
-      factoid.setLocked(true);
-      dao.save(factoid);
-      responses.add(new Message(event.getChannel(), event, name + " locked."));
-    } else if ("unlock".equals(command)) {
-      factoid.setLocked(false);
-      dao.save(factoid);
-      responses.add(new Message(event.getChannel(), event, name + " unlocked."));
+    if ("lock".equals(command) || "unlock".equals(command)) {
+      final Factoid factoid = dao.getFactoid(name);
+      if(factoid == null) {
+        responses.add(new Message(event.getChannel(), event, "I don't know anything about " + name));
+      } else if ("lock".equals(command)) {
+        factoid.setLocked(true);
+        dao.save(factoid);
+        responses.add(new Message(event.getChannel(), event, name + " locked."));
+      } else if ("unlock".equals(command)) {
+        factoid.setLocked(false);
+        dao.save(factoid);
+        responses.add(new Message(event.getChannel(), event, name + " unlocked."));
+      }
     }
     return responses;
   }
