74b27b6802660ff77d377fce3ae92a3937164e1d
evanchooly <jlee@antwerkz.com>
2013-11-30 09:15:00 -0500

    tweaks. clean ups.

--- core/src/main/java/javabot/operations/JSROperation.java
+++ core/src/main/java/javabot/operations/JSROperation.java
@@ -1,23 +1,24 @@
 package javabot.operations;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+
 import com.antwerkz.maven.SPI;
 import javabot.IrcEvent;
 import javabot.Message;
 import javabot.operations.locator.JCPJSRLocator;
-import javabot.operations.locator.impl.JCPJSRLocatorImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SPI(BotOperation.class)
 public class JSROperation extends BotOperation {
-    JCPJSRLocator locator = new JCPJSRLocatorImpl();
+    @Inject
+    JCPJSRLocator locator;
 
     @Override
     public List<Message> handleMessage(final IrcEvent event) {
         final String message = event.getMessage().toLowerCase();
         final String channel = event.getChannel();
-        final List<Message> responses = new ArrayList<Message>();
+        final List<Message> responses = new ArrayList<>();
         if ("jsr".equals(message)) {
             responses.add(new Message(channel, event, "Please supply a JSR number to look up."));
         } else {
@@ -27,7 +28,7 @@ public class JSROperation extends BotOperation {
                 try {
                     final int jsr = Integer.parseInt(jsrString);
                     String response = locator.findInformation(jsr);
-                    if (!"".equals(response)) {
+                    if (response != null && !response.isEmpty()) {
                         responses.add(new Message(channel, event, response));
                     } else {
                         responses.add(new Message(channel, event, "I'm sorry, I can't find a JSR " + jsrString));
--- core/src/main/java/javabot/operations/locator/Locator.java
+++
@@ -1,7 +0,0 @@
-package javabot.operations.locator;
-
-import java.util.Map;
-
-public interface Locator {
-	Map<String, String> locate(Map<String, String> inputs);
-}
