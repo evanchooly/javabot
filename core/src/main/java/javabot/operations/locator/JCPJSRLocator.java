8d6217d6db718ecca4b4c38e360be1d99f78b1f9
jottinger <joeo@enigmastation.com>
2013-12-03 13:05:00 -0500

    yanking NekoHTML and the code that uses it back to the heck in which it belongs. Replaced with JSoup code, and it's leaner now. I think.

--- core/src/main/java/javabot/operations/locator/JCPJSRLocator.java
+++ core/src/main/java/javabot/operations/locator/JCPJSRLocator.java
@@ -1,91 +1,67 @@
 package javabot.operations.locator;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.URL;
-import java.net.URLConnection;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import com.google.inject.ImplementedBy;
-import org.cyberneko.html.parsers.DOMParser;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
 public class JCPJSRLocator {
-  public Map<String, String> locate(final Map<String, String> inputs) {
-      final Map<String, String> retVal = new HashMap<String, String>();
-      final String urlString = "http://www.jcp.org/en/jsr/detail?id=" + inputs.get("jsr");
-      retVal.put("url", urlString);
-      final URL url;
-      try {
-          BufferedReader reader = null;
-          try {
-              url = new URL(urlString);
-              final URLConnection connection = url.openConnection();
-              connection.setConnectTimeout(1000);
-              connection.setReadTimeout(1000);
-              reader = new BufferedReader(new InputStreamReader((InputStream) url.getContent()));
-              final StringBuilder builder = new StringBuilder();
-              while (!builder.toString().trim().endsWith("</html>")) {
-                  final String line = reader.readLine();
-                  if (line != null) {
-                      builder.append(line);
-                  }
-              }
-              final DOMParser docBuilder = new DOMParser();
-              docBuilder.parse(new InputSource(new StringReader(builder.toString().trim())));
-              final Document dom = docBuilder.getDocument();
-              final NodeList nodes = dom.getElementsByTagName("div");
-              for (int i = 0; i < nodes.getLength(); i++) {
-                  final Node div = nodes.item(i);
-                  final NamedNodeMap nnm = div.getAttributes();
-                  if (nnm.getNamedItem("class") != null) {
-                      if ("header1".equals(nnm.getNamedItem("class").getNodeValue())) {
-                          retVal.put("title", getNodeValue(div));
-                          break;
-                      }
-                  }
-              }
-          } finally {
-              if (reader != null) {
-                  reader.close();
-              }
-          }
+    public Map<String, String> locate(final Map<String, String> inputs) {
+        final Map<String, String> retVal = new HashMap<>();
+        final String urlString = "http://www.jcp.org/en/jsr/detail?id=" + inputs.get("jsr");
+        retVal.put("url", urlString);
+        try {
+            HttpParams params = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(params, 5 * 1000);
+            HttpConnectionParams.setSoTimeout(params, 5 * 1000);
+            HttpClient httpclient = new DefaultHttpClient(params);
+            HttpGet httpget = new HttpGet(urlString);
+            HttpResponse response = httpclient.execute(httpget);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                try {
+                    String data = EntityUtils.toString(entity);
+                    org.jsoup.nodes.Document doc = Jsoup.parse(data);
+                    Element title = doc.select("div.header1").first();
+                    StringBuilder titleText = new StringBuilder();
+                    String separator = "";
+                    // we build the content,
+                    // because of that stupid <sup> thing the JCP uses
+                    for (TextNode element : title.textNodes()) {
+                        titleText.append(separator)
+                                .append(element.text().trim());
+                        separator = " ";
+                    }
+                    retVal.put("title", titleText.toString());
+                } finally {
+                    EntityUtils.consume(entity);
+                }
+            }
 
-      } catch (Exception e) {
-          retVal.put("title", "Could not determine JSR information");
-          e.printStackTrace();
-      }
-      return retVal;
-  }
+        } catch (Exception e) {
+            retVal.put("title", null);
+        }
+        return retVal;
+    }
 
-  private String getNodeValue(final Node node) {
-      final StringBuilder value = new StringBuilder();
-      final int length = node.getChildNodes().getLength();
-      for (int index = 0; index < length; index++) {
-          final Node child = node.getChildNodes().item(index);
-          if (child.getChildNodes().getLength() != 0) {
-              value.append(getNodeValue(child));
-          } else {
-              value.append(child.getNodeValue());
-          }
-      }
-      return value.toString();
-  }
-
-  public String findInformation(final int jsr) {
-      final Map<String, String> inputs = new HashMap<String, String>();
-      inputs.put("jsr", Integer.toString(jsr));
-      final Map<String, String> outputs = locate(inputs);
-      if(outputs.get("title")==null) {
-          return "";
-      }
-      return "'" + outputs.get("title") + "' can be found at " + outputs.get("url");
-  }
+    public String findInformation(final int jsr) {
+        final Map<String, String> inputs = new HashMap<>();
+        inputs.put("jsr", Integer.toString(jsr));
+        final Map<String, String> outputs = locate(inputs);
+        if (outputs.get("title") == null) {
+            return "";
+        }
+        return "'" + outputs.get("title") + "' can be found at " + outputs.get("url");
+    }
 }
