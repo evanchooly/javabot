db23b114e04c2026b0675e423c12a69d9d10bcfc
jottinger <joeo@enigmastation.com>
2013-12-03 13:05:00 -0500

    yanking NekoHTML and the code that uses it back to the heck in which it belongs. Replaced with JSoup code, and it's leaner now. I think.

--- core/src/test/java/javabot/operations/JCPJSFLocatorImplTest.java
+++ core/src/test/java/javabot/operations/JCPJSFLocatorImplTest.java
@@ -9,6 +9,6 @@ public class JCPJSFLocatorImplTest {
     public void jsr315() {
         final JCPJSRLocator locator = new JCPJSRLocator();
         Assert.assertEquals(locator.findInformation(315),
-            "'JSR 315: JavaTM Servlet 3.0 Specification' can be found at http://www.jcp.org/en/jsr/detail?id=315");
+            "'JSR 315: Java Servlet 3.0 Specification' can be found at http://www.jcp.org/en/jsr/detail?id=315");
     }
 }
--- core/src/test/java/javabot/operations/JSROperationTest.java
+++ core/src/test/java/javabot/operations/JSROperationTest.java
@@ -2,6 +2,7 @@ package javabot.operations;
 
 import com.google.inject.Inject;
 import javabot.operations.locator.JCPJSRLocator;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertNotNull;
@@ -18,7 +19,7 @@ public class JSROperationTest extends BaseOperationTest {
 
     @Test
     public void testJSROperations() {
-        testMessage("~jsr 220", "'JSR 220: Enterprise JavaBeansTM 3.0' "
+        testMessage("~jsr 220", "'JSR 220: Enterprise JavaBeans 3.0' "
                 + "can be found at http://www.jcp.org/en/jsr/detail?id=220");
     }
 
@@ -27,11 +28,18 @@ public class JSROperationTest extends BaseOperationTest {
         testMessage("~jsr 2202213", "I'm sorry, I can't find a JSR 2202213");
     }
 
-    @Test
-    public void testNullJSROperations() {
-        testMessage("~jsr", "Please supply a JSR number to look up.");
-        testMessage("~jsr ", "Please supply a JSR number to look up.");
-        testMessage("~jsr abc", "'abc' is not a valid JSR reference.");
+    @DataProvider(name = "badCommands")
+    private Object[][] badCommands() {
+        return new Object[][]{
+                {"~jsr", "Please supply a JSR number to look up."},
+                {"~jsr ", "Please supply a JSR number to look up."},
+                {"~jsr abc", "'abc' is not a valid JSR reference."},
+        };
+    }
+
+    @Test(dataProvider = "badCommands")
+    public void testNullJSROperations(String command, String result) {
+        testMessage(command, result);
     }
 
 }
