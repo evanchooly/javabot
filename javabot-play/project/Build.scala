826d223d03260bc97d52b90dfe77b67d461181a2
evanchooly <jlee@antwerkz.com>
2014-04-02 22:51:47 -0400

    play app updates.  maybe it'll start working again.

--- javabot-play/project/Build.scala
+++ javabot-play/project/Build.scala
@@ -4,16 +4,16 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "javabot"
-  val appVersion      = "5.1.1-SNAPSHOT"
+  val name = "javabot"
+  val version = "5.1.1-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.webjars" % "webjars-play" % "2.1.0-1",
-    "org.webjars" % "bootstrap" % "2.3.1",
+    "org.webjars" %% "webjars-play" % "2.2.1-2",
+    "org.webjars" % "bootstrap" % "3.1.0",
     "org.webjars" % "jquery" % "1.9.1",
-    "org.webjars" % "less" % "1.3.3",
-    "com.antwerkz.javabot" % "core" % appVersion changing(),
-    "be.objectify" %% "deadbolt-scala" % "2.1-RC2",
+    "org.webjars" % "less" % "1.7.0",
+    "com.antwerkz.javabot" % "core" % version changing(),
+    "be.objectify" %% "deadbolt-scala" % "2.2-RC2",
     "com.google.inject" % "guice" % "3.0",
     "com.google.inject.extensions" % "guice-servlet" % "3.0",
     "com.google.inject.extensions" % "guice-multibindings" % "3.0",
@@ -24,8 +24,8 @@ object ApplicationBuild extends Build {
     "com.codiform" % "moo" % "1.3"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+  val main = play.Project(name, version, appDependencies).settings(
+    resolvers += "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository",
     resolvers += Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
     resolvers += Resolver.url("Objectify Play Snapshot Repository", url("http://schaloner.github.com/snapshots/"))(Resolver.ivyStylePatterns),
     resolvers += "Sonatype snapshots repository" at "https://oss.sonatype.org/content/repositories/public/",
