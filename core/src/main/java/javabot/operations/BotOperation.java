2a954a50106bdd155637488cf58750a8610b7101
Joseph B. Ottinger <joeo@enigmastation.com>
2014-12-17 13:32:02 -0500

    Adds message priority to factoids such that factoids can be set to be low priority

--- core/src/main/java/javabot/operations/BotOperation.java
+++ core/src/main/java/javabot/operations/BotOperation.java
@@ -20,6 +20,14 @@ public abstract class BotOperation {
     }
 
     /**
+     * This method returns where the operation should fall in terms of priority. Lower values represent lower
+     * priority. Implementations can probably get by with the default implementation (which returns a priority of 10)
+     * but some commands may want to override this.
+     * @return the priority of the command
+     */
+    public int getPriority() { return 10;}
+
+    /**
      * @return true if the message has been handled
      */
     public boolean handleMessage(final Message event) {
--- core/src/main/java/javabot/operations/OperationComparator.java
+++ core/src/main/java/javabot/operations/OperationComparator.java
@@ -5,6 +5,10 @@ import java.util.Comparator;
 public class OperationComparator implements Comparator<BotOperation> {
     @Override
     public int compare(final BotOperation botOperation, final BotOperation o) {
-        return botOperation.getName().compareTo(o.getName());
+        int value=Integer.compare(o.getPriority(), botOperation.getPriority());
+        if(value==0) {
+            value = botOperation.getName().compareTo(o.getName());
+        }
+        return value;
     }
 }
