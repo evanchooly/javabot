50df99c0d77ddb29e52050f3a46dd22bccf4750c
evanchooly <jlee@antwerkz.com>
2015-02-11 20:10:04 -0500

    moar logs!

--- core/src/main/java/javabot/dao/AdminDao.java
+++ core/src/main/java/javabot/dao/AdminDao.java
@@ -1,5 +1,9 @@
 package javabot.dao;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.inject.Inject;
+
 import javabot.model.Admin;
 import javabot.model.Config;
 import javabot.model.EventType;
@@ -7,90 +11,89 @@ import javabot.model.OperationEvent;
 import javabot.model.criteria.AdminCriteria;
 import org.pircbotx.User;
 
-import javax.inject.Inject;
-import java.time.LocalDateTime;
-import java.util.List;
-
 public class AdminDao extends BaseDao<Admin> {
-    @Inject
-    private ConfigDao configDao;
+  @Inject
+  private ConfigDao configDao;
 
-    public AdminDao() {
-        super(Admin.class);
+  public AdminDao() {
+    super(Admin.class);
+  }
+
+  @Override
+  public List<Admin> findAll() {
+    System.out.println("AdminDao.findAll");
+    return ds.createQuery(Admin.class).order("userName").asList();
+  }
+
+  public boolean isAdmin(final User user) {
+    return findAll().isEmpty() || getAdmin(user) != null;
+  }
+
+  public Admin getAdmin(final String ircName, final String hostName) {
+    AdminCriteria adminCriteria = new AdminCriteria(ds);
+    adminCriteria.ircName().equal(ircName);
+    adminCriteria.hostName().equal(hostName);
+    return adminCriteria.query().get();
+  }
+
+  public Admin getAdmin(final User user) {
+    System.out.println("AdminDao.getAdmin");
+    System.out.println("user = [" + user + "]");
+    AdminCriteria adminCriteria = new AdminCriteria(ds);
+    adminCriteria.ircName(user.getNick());
+    return adminCriteria.query().get();
+  }
+
+  public Admin getAdminByEmailAddress(final String email) {
+    AdminCriteria criteria = new AdminCriteria(ds);
+    criteria.emailAddress(email);
+    Admin admin = criteria.query().get();
+    if (admin == null && count() == 0) {
+      admin = new Admin();
+      admin.setEmailAddress(email);
+      admin.setBotOwner(true);
+      save(admin);
     }
+    return admin;
+  }
 
-    @Override
-    public List<Admin> findAll() {
-        return ds.createQuery(Admin.class).order("userName").asList();
-    }
+  public Admin create(final String ircName, final String userName, final String hostName) {
+    final Admin admin = new Admin();
+    admin.setIrcName(ircName);
+    admin.setEmailAddress(userName);
+    admin.setHostName(hostName);
+    admin.setUpdated(LocalDateTime.now());
+    admin.setBotOwner(findAll().isEmpty());
+    save(admin);
 
-    public boolean isAdmin(final User user) {
-        return findAll().isEmpty() || getAdmin(user) != null;
-    }
+    return admin;
+  }
 
-    public Admin getAdmin(final String ircName, final String hostName) {
-        AdminCriteria adminCriteria = new AdminCriteria(ds);
-        adminCriteria.ircName().equal(ircName);
-        adminCriteria.hostName().equal(hostName);
-        return adminCriteria.query().get();
-    }
+  public void enableOperation(String name, Admin admin) {
+    OperationEvent event = new OperationEvent();
+    event.setOperation(name);
+    event.setRequestedOn(LocalDateTime.now());
+    event.setType(EventType.ADD);
+    event.setRequestedBy(admin.getEmailAddress());
+    save(event);
+    Config config = configDao.get();
+    config.getOperations().add(name);
+    configDao.save(config);
+  }
 
-    public Admin getAdmin(final User user) {
-        AdminCriteria adminCriteria = new AdminCriteria(ds);
-        adminCriteria.ircName(user.getNick());
-        return adminCriteria.query().get();
-    }
+  public void disableOperation(String name, Admin admin) {
+    OperationEvent event = new OperationEvent();
+    event.setOperation(name);
+    event.setRequestedOn(LocalDateTime.now());
+    event.setType(EventType.DELETE);
+    event.setRequestedBy(admin.getEmailAddress());
+    save(event);
+    Config config = configDao.get();
+    config.getOperations().remove(name);
+    configDao.save(config);
+  }
 
-    public Admin getAdminByEmailAddress(final String email) {
-        AdminCriteria criteria = new AdminCriteria(ds);
-        criteria.emailAddress(email);
-        Admin admin = criteria.query().get();
-        if(admin == null && count() == 0) {
-            admin = new Admin();
-            admin.setEmailAddress(email);
-            admin.setBotOwner(true);
-            save(admin);
-        }
-        return admin;
-    }
-
-    public Admin create(final String ircName, final String userName, final String hostName) {
-        final Admin admin = new Admin();
-        admin.setIrcName(ircName);
-        admin.setEmailAddress(userName);
-        admin.setHostName(hostName);
-        admin.setUpdated(LocalDateTime.now());
-        admin.setBotOwner(findAll().isEmpty());
-        save(admin);
-
-        return admin;
-    }
-
-    public void enableOperation(String name, Admin admin) {
-        OperationEvent event = new OperationEvent();
-        event.setOperation(name);
-        event.setRequestedOn(LocalDateTime.now());
-        event.setType(EventType.ADD);
-        event.setRequestedBy(admin.getEmailAddress());
-        save(event);
-        Config config = configDao.get();
-        config.getOperations().add(name);
-        configDao.save(config);
-    }
-
-    public void disableOperation(String name, Admin admin) {
-        OperationEvent event = new OperationEvent();
-        event.setOperation(name);
-        event.setRequestedOn(LocalDateTime.now());
-        event.setType(EventType.DELETE);
-        event.setRequestedBy(admin.getEmailAddress());
-        save(event);
-        Config config = configDao.get();
-        config.getOperations().remove(name);
-        configDao.save(config);
-    }
-
-    public long count() {
-        return getQuery().countAll();
-    }
+  public long count() {
+    return getQuery().countAll();
+  }
 }
--- core/src/main/java/javabot/operations/throttle/Throttler.java
+++ core/src/main/java/javabot/operations/throttle/Throttler.java
@@ -1,8 +1,15 @@
 package javabot.operations.throttle;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.inject.Inject;
+import javax.inject.Provider;
+
 import com.antwerkz.sofia.Sofia;
 import com.jayway.awaitility.Awaitility;
 import com.jayway.awaitility.core.ConditionTimeoutException;
+import static java.time.LocalDateTime.now;
 import javabot.dao.AdminDao;
 import javabot.dao.BaseDao;
 import javabot.dao.ConfigDao;
@@ -13,71 +20,66 @@ import javabot.model.criteria.ThrottleItemCriteria;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 
-import javax.inject.Inject;
-import javax.inject.Provider;
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static java.time.LocalDateTime.now;
-
 public class Throttler extends BaseDao<ThrottleItem> {
-    @Inject
-    private ConfigDao configDao;
+  @Inject
+  private ConfigDao configDao;
 
-    @Inject
-    private AdminDao adminDao;
+  @Inject
+  private AdminDao adminDao;
 
-    @Inject
-    private NickServDao nickServDao;
+  @Inject
+  private NickServDao nickServDao;
 
-    @Inject
-    private Provider<PircBotX> ircBot;
+  @Inject
+  private Provider<PircBotX> ircBot;
 
-    protected Throttler() {
-        super(ThrottleItem.class);
+  protected Throttler() {
+    super(ThrottleItem.class);
+  }
+
+  /**
+   * Check if a user is currently throttled or not.
+   *
+   * @return true if the user is currently throttled and ought to be ignored, false otherwise.
+   */
+  public boolean isThrottled(final User user) {
+    System.out.println("checking isAdmin");
+    if (!adminDao.isAdmin(user)) {
+      System.out.println("validateNickServAccount");
+      validateNickServAccount(user);
+      ds.save(new ThrottleItem(user));
+      ThrottleItemCriteria criteria = new ThrottleItemCriteria(ds);
+      criteria.user(user.getNick());
+      System.out.println("counting throttles");
+      return criteria.query().countAll() > configDao.get().getThrottleThreshold();
     }
+    return false;
+  }
 
-    /**
-     * Check if a user is currently throttled or not.
-     *
-     * @return true if the user is currently throttled and ought to be ignored, false otherwise.
-     */
-    public boolean isThrottled(final User user) {
-        if (!adminDao.isAdmin(user)) {
-            validateNickServAccount(user);
-            ds.save(new ThrottleItem(user));
-            ThrottleItemCriteria criteria = new ThrottleItemCriteria(ds);
-            criteria.user(user.getNick());
-            return criteria.query().countAll() > configDao.get().getThrottleThreshold();
-        }
-        return false;
+  private void validateNickServAccount(final User user) {
+    AtomicReference<NickServInfo> info = new AtomicReference<>(nickServDao.find(user.getNick()));
+    if (info.get() == null) {
+      ircBot.get().sendIRC().message("NickServ", "info " + user.getNick());
+      Sofia.logWaitingForNickserv(user.getNick());
+      try {
+        Awaitility.await()
+            .atMost(15, TimeUnit.SECONDS)
+            .until(() -> {
+              info.set(nickServDao.find(user.getNick()));
+              return info.get() != null;
+            });
+      } catch (ConditionTimeoutException e) {
+        Sofia.logNoNickservEntry(user.getNick());
+        throw new NickServViolationException(Sofia.unknownUser());
+      }
     }
-
-    private void validateNickServAccount(final User user) {
-        AtomicReference<NickServInfo> info = new AtomicReference<>(nickServDao.find(user.getNick()));
-        if (info.get() == null) {
-            ircBot.get().sendIRC().message("NickServ", "info " + user.getNick());
-            Sofia.logWaitingForNickserv(user.getNick());
-            try {
-                Awaitility.await()
-                          .atMost(15, TimeUnit.SECONDS)
-                          .until(() -> {
-                              info.set(nickServDao.find(user.getNick()));
-                              return info.get() != null;
-                          });
-            } catch (ConditionTimeoutException e) {
-                Sofia.logNoNickservEntry(user.getNick());
-                throw new NickServViolationException(Sofia.unknownUser());
-            }
-        }
-        NickServInfo nickServInfo = info.get();
-        if (nickServInfo == null) {
-            Sofia.logNoNickservEntry(user.getNick());
-            throw new NickServViolationException(Sofia.unknownUser());
-        }
-        if (Duration.between(nickServInfo.getRegistered(), now()).toDays() < configDao.get().getMinimumNickServAge()) {
-            throw new NickServViolationException(Sofia.accountTooNew());
-        }
+    NickServInfo nickServInfo = info.get();
+    if (nickServInfo == null) {
+      Sofia.logNoNickservEntry(user.getNick());
+      throw new NickServViolationException(Sofia.unknownUser());
     }
+    if (Duration.between(nickServInfo.getRegistered(), now()).toDays() < configDao.get().getMinimumNickServAge()) {
+      throw new NickServViolationException(Sofia.accountTooNew());
+    }
+  }
 }
