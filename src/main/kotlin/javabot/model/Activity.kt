1bd59763c6a4454ae87aaac8216b9bba0bb56c2e
evanchooly <jlee@antwerkz.com>
2015-12-25 20:38:27 -0500

    remove @Entity

--- src/main/kotlin/javabot/model/Activity.kt
+++ src/main/kotlin/javabot/model/Activity.kt
@@ -5,7 +5,6 @@ import java.io.Serializable
 import java.text.DecimalFormat
 import java.time.LocalDateTime
 
-@Entity
 class Activity(val channel: String, val count: Long, val end: LocalDateTime,
                val start: LocalDateTime, var total: Long) : Serializable {
 
