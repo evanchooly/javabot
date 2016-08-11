732c2d90dbc7e6cbebfe348b81564cb7a4fcd325
Joseph B. Ottinger <joeo@enigmastation.com>
2016-08-11 12:34:37 -0400

    Modifying VersionOperation to use Git tag information if available

---
+++ src/test/kotlin/javabot/operations/VersionOperationTest.kt
@@ -0,0 +1,16 @@
+package javabot.operations
+
+import com.google.inject.Inject
+import javabot.BaseTest
+import org.testng.annotations.Test
+
+class VersionOperationTest : BaseTest() {
+    @Inject
+    private lateinit var operation: VersionOperation
+
+    /** We expect the test to not be able to find the manifest resource. */
+    @Test
+    fun tellVersion() {
+        scanForResponse(operation.handleMessage(message("~version")), "I am currently running from git tag UNKNOWN")
+    }
+}
