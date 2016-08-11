5e8deed6a5841a7805887e514015339c891018f6
Joseph B. Ottinger <joeo@enigmastation.com>
2016-08-11 13:35:17 -0400

    Trying to work out a way to test this stuff

--- src/main/kotlin/javabot/operations/VersionOperation.kt
+++ src/main/kotlin/javabot/operations/VersionOperation.kt
@@ -1,42 +1,42 @@
 package javabot.operations
 
-import ca.grimoire.maven.ArtifactDescription
-import ca.grimoire.maven.NoArtifactException
 import com.antwerkz.sofia.Sofia
 import com.google.inject.Inject
 import javabot.Javabot
 import javabot.Message
 import javabot.dao.AdminDao
-import java.io.File
-import java.io.FileInputStream
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import java.util.jar.Manifest
 
 class VersionOperation @Inject constructor(bot: Javabot, adminDao: AdminDao) : BotOperation(bot, adminDao), StandardOperation {
+    var lastInvocationTime = LocalDateTime.of(1992, 10, 17, 9, 0)
+
     override fun handleMessage(event: Message): List<Message> {
         val responses = arrayListOf<Message>()
         val message = event.value
         if ("version".equals(message, ignoreCase = true)) {
-            responses.add(Message(event, Sofia.botVersion(loadVersion())))
+            if (LocalDateTime.now() > lastInvocationTime.plus(5, ChronoUnit.MINUTES)) {
+                lastInvocationTime=LocalDateTime.now()
+
+                responses.add(Message(event, Sofia.botVersion(loadVersion())))
+            }
         }
         return responses
     }
 
     fun loadVersion(): String {
-        try {
-            return ArtifactDescription.locate("javabot", "core").version
-        } catch (nae: NoArtifactException) {
-            try {
-                val file = File("target/maven-archiver/pom.properties")
-                if (file.exists()) {
-                    return ArtifactDescription.locate("javabot", "core", { FileInputStream(file) }).version
-                } else {
-                    return "UNKNOWN"
-                }
-            } catch (e: NoArtifactException) {
-                return "UNKNOWN"
-            }
+        val mf = Manifest();
+        val manifestResource=Thread.currentThread().getContextClassLoader().getResourceAsStream("/META-INF/MANIFEST.MF")
+        if(manifestResource!=null) {
+            mf.read(manifestResource);
 
+            val atts = mf.getMainAttributes();
+
+            return atts.getValue("Implementation-Build");
+        } else {
+            return "UNKNOWN"
         }
-
     }
 
 }
