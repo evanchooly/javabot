fc0e462795db0bcba9a3bd87a293c435c99f5bb7
Joseph B. Ottinger <joeo@enigmastation.com>
2016-11-17 08:43:54 -0500

    Fixing * import

--- src/test/kotlin/javabot/operations/GetFactoidOperationTest.kt
+++ src/test/kotlin/javabot/operations/GetFactoidOperationTest.kt
@@ -7,7 +7,7 @@ import org.testng.Assert
 import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
-import java.util.*
+import java.util.Arrays
 import javax.inject.Inject
 
 @Test class GetFactoidOperationTest : BaseTest() {
