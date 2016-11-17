debe3ea7890dd6d26c412739d7c1549d2abde41c
Joseph B. Ottinger <joeo@enigmastation.com>
2016-11-17 13:19:16 -0500

    Cleaning up RFC retrieval code to not suck so badly

--- src/main/kotlin/javabot/operations/RFCOperation.kt
+++ src/main/kotlin/javabot/operations/RFCOperation.kt
@@ -3,13 +3,16 @@ package javabot.operations
 import com.antwerkz.sofia.Sofia
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
+import com.google.common.cache.LoadingCache
 import com.google.inject.Inject
 import com.google.inject.Singleton
 import javabot.Javabot
 import javabot.Message
 import javabot.dao.AdminDao
+import org.jsoup.HttpStatusException
 import org.jsoup.Jsoup
 import java.io.IOException
+import java.net.UnknownHostException
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 
@@ -17,46 +20,56 @@ import java.util.concurrent.TimeUnit
  * Displays RFC url and title
  */
 @Singleton class RFCOperation @Inject constructor(bot: Javabot, adminDao: AdminDao) : BotOperation(bot, adminDao) {
-    val rfcServerList=listOf("http://www.rfc-base.org/rfc-%d.html","https://tools.ietf.org/html/rfc%d")
+    val rfcServerList = listOf("http://www.rfc-base.org/rfc-%d.html", "https://tools.ietf.org/html/rfc%d")
+    val rfcBadServers = mutableListOf<String>()
 
-    var rfcTitleCache = CacheBuilder.newBuilder().maximumSize(100).expireAfterWrite(1, TimeUnit.HOURS).recordStats().build(
-          object : CacheLoader<String, String>() {
-              @SuppressWarnings("NullableProblems")
-              @Throws(IOException::class)
-              override fun load(url: String): String {
-                  val doc = Jsoup.connect(url).get()
-                  return doc.title()
-              }
-          })
+    val rfcTitleCache: LoadingCache<Int, Pair<String, String>> =
+            CacheBuilder.newBuilder().maximumSize(100).expireAfterWrite(1, TimeUnit.HOURS).recordStats().build(
+                    object : CacheLoader<Int, Pair<String, String>>() {
+                        @SuppressWarnings("NullableProblems")
+                        @Throws(IOException::class)
+                        override fun load(rfc: Int): Pair<String, String> {
+                            var title: String? = null
+                            var url: String? = null
+                            rfcServerList
+                                    .filterNot { rfcBadServers.contains(it) }
+                                    .forEach {
+                                        // how I wish return@label worked in the lambda...
+                                        if (title == null) {
+                                            url = it.format(rfc)
+                                            try {
+                                                val doc = Jsoup.connect(url).get()
+                                                title = doc.title()
+                                                //} catch(e: ExecutionException) {
+                                            } catch(e: UnknownHostException) {
+                                                rfcBadServers.add(it)
+                                            } catch(e: HttpStatusException) {
+                                            }
+                                        }
+                                    }
+                            if (title == null) {
+                                throw IOException("RFC $rfc does not resolve")
+                            }
+                            return Pair(url.orEmpty(), title.orEmpty())
+                        }
+                    })
 
     override fun handleMessage(event: Message): List<Message> {
         val responses = arrayListOf<Message>()
         val message = event.value.toLowerCase()
         if (message.startsWith(prefix)) {
             val rfcText = message.substring(prefix.length).trim()
-            if(rfcText.isEmpty()) {
+            if (rfcText.isEmpty()) {
                 responses.add(Message(event, Sofia.rfcMissing()))
             } else {
                 try {
                     val rfc = Integer.parseInt(rfcText)
-                    var found=false
-                    rfcServerList.forEach {
-                        // how I wish return@label worked in the lambda...
-                        if(!found) {
-                            val url = it.format(rfc)
-                            try {
-                                val data = rfcTitleCache.get(url)
-                                responses.add(Message(event, Sofia.rfcSucceed(url, data)))
-                                found = true
-                            } catch(e: ExecutionException) {
-                            }
-                        }
-                    }
-                    if(!found) {
-                        responses.add(Message(event, Sofia.rfcFail(rfcText)))
-                    }
+                    val data = rfcTitleCache.get(rfc)
+                    responses.add(Message(event, Sofia.rfcSucceed(data.first, data.second)))
                 } catch (e: NumberFormatException) {
                     responses.add(Message(event, Sofia.rfcInvalid(rfcText)))
+                } catch (e: ExecutionException) {
+                    responses.add(Message(event, Sofia.rfcFail(rfcText)))
                 }
             }
         }
