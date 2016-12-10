d86b01064dabc9bbabb6ce63c370b563de12b102
evanchooly <jlee@antwerkz.com>
2016-12-09 19:44:19 -0500

    remove the rfc caching.  use the one server.

--- src/main/kotlin/javabot/operations/RFCOperation.kt
+++ src/main/kotlin/javabot/operations/RFCOperation.kt
@@ -1,48 +1,19 @@
 package javabot.operations
 
 import com.antwerkz.sofia.Sofia
-import com.google.common.cache.CacheBuilder
-import com.google.common.cache.CacheLoader
-import com.google.common.cache.LoadingCache
 import com.google.inject.Inject
 import com.google.inject.Singleton
 import javabot.Javabot
 import javabot.Message
 import javabot.dao.AdminDao
+import org.jsoup.HttpStatusException
 import org.jsoup.Jsoup
 import java.io.IOException
-import java.net.UnknownHostException
-import java.util.concurrent.ExecutionException
-import java.util.concurrent.TimeUnit
 
 /**
  * Displays RFC url and title
  */
 @Singleton class RFCOperation @Inject constructor(bot: Javabot, adminDao: AdminDao) : BotOperation(bot, adminDao) {
-    val rfcServerList = listOf("http://www.rfc-base.org/rfc-%d.html", "https://tools.ietf.org/html/rfc%d")
-    val rfcBadServers = mutableListOf<String>()
-
-    val rfcTitleCache: LoadingCache<Int, Pair<String, String>> =
-            CacheBuilder.newBuilder().maximumSize(100).expireAfterWrite(1, TimeUnit.HOURS).recordStats().build(
-                    object : CacheLoader<Int, Pair<String, String>>() {
-                        @SuppressWarnings("NullableProblems")
-                        override fun load(rfc: Int): Pair<String, String> {
-                            rfcServerList
-                                    .filterNot { rfcBadServers.contains(it) }
-                                    .forEach {
-                                        // how I wish return@label worked in the lambda...
-                                        val url = it.format(rfc)
-                                        try {
-                                            val doc = Jsoup.connect(url).get()
-                                            return Pair(url, doc.title())
-                                        } catch(e: UnknownHostException) {
-                                            rfcBadServers.add(it)
-                                        }
-                                    }
-                            throw IOException("RFC $rfc not retrievable")
-                        }
-                    })
-
     override fun handleMessage(event: Message): List<Message> {
         val responses = arrayListOf<Message>()
         val message = event.value.toLowerCase()
@@ -52,17 +23,24 @@ import java.util.concurrent.TimeUnit
                 responses.add(Message(event, Sofia.rfcMissing()))
             } else {
                 try {
-                    val rfc = Integer.parseInt(rfcText)
-                    val data = rfcTitleCache.get(rfc)
+                    val data = load(Integer.parseInt(rfcText))
                     responses.add(Message(event, Sofia.rfcSucceed(data.first, data.second)))
                 } catch (e: NumberFormatException) {
                     responses.add(Message(event, Sofia.rfcInvalid(rfcText)))
-                } catch (e: ExecutionException) {
+                } catch (e: HttpStatusException) {
+                    responses.add(Message(event, Sofia.rfcFail(rfcText)))
+                } catch (e: IOException) {
                     responses.add(Message(event, Sofia.rfcFail(rfcText)))
                 }
             }
         }
         return responses
+    }
+
+    fun load(rfc: Int): Pair<String, String> {
+        val url = "https://tools.ietf.org/html/rfc$rfc"
+        val doc = Jsoup.connect(url).get()
+        return Pair(url, doc.title())
     }
 
     companion object {
--- src/test/kotlin/javabot/operations/RFCOperationTest.kt
+++ src/test/kotlin/javabot/operations/RFCOperationTest.kt
@@ -3,20 +3,21 @@ package javabot.operations
 import com.antwerkz.sofia.Sofia
 import javabot.BaseTest
 import org.testng.Assert
-import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 import javax.inject.Inject
 
 @Test(groups = arrayOf("operations")) class RFCOperationTest : BaseTest() {
     @Inject
-    protected lateinit var operation: RFCOperation
+    private lateinit var operation: RFCOperation
 
-    @Test fun testBadRFCNumber() {
+    @Test
+    fun testBadRFCNumber() {
         val response = operation.handleMessage(message("~rfc abd"))
         Assert.assertEquals(response[0].value, Sofia.rfcInvalid("abd"))
     }
 
-    @Test fun testMissingRFCNumber() {
+    @Test
+    fun testMissingRFCNumber() {
         Assert.assertEquals(operation.handleMessage(message("~rfc "))[0].value, Sofia.rfcMissing())
         Assert.assertEquals(operation.handleMessage(message("~rfc"))[0].value, Sofia.rfcMissing())
     }
@@ -25,17 +26,16 @@ import javax.inject.Inject
         return m.contains(r)
     }
 
-    @Test fun testHTTPRFC() {
+    @Test
+    fun testHTTPRFC() {
         val possibleResponses = listOf(
                 Sofia.rfcSucceed("http://www.rfc-base.org/rfc-2616.html", "RFC 2616 - Hypertext Transfer Protocol HTTP/1.1"),
                 Sofia.rfcSucceed("https://tools.ietf.org/html/rfc2616", "RFC 2616 - Hypertext Transfer Protocol -- HTTP/1.1")
         )
-        val hitRate = operation.rfcTitleCache.stats().hitCount()
         var response = operation.handleMessage(message("~rfc 2616"))
-        Assert.assertTrue(matches(response[0].value,possibleResponses))
+        Assert.assertTrue(matches(response[0].value,possibleResponses), "$response")
         response = operation.handleMessage(message("~rfc 2616"))
         Assert.assertTrue(matches(response[0].value,possibleResponses))
-        assertEquals(operation.rfcTitleCache.stats().hitCount(), hitRate + 1)
     }
 
     fun testNonexistentRFC() {
