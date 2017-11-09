8fa8fe5a4b72d7d25962957b6879da8d8601b847
Joseph B. Ottinger <joseph.ottinger@meltwater.com>
2017-11-09 06:29:16 -0500

    Adding test for 'say' when not on channel

--- src/test/kotlin/javabot/operations/SayOperationsTest.kt
+++ src/test/kotlin/javabot/operations/SayOperationsTest.kt
@@ -2,10 +2,12 @@ package javabot.operations
 
 import com.google.inject.Inject
 import javabot.BaseTest
+import javabot.MockIrcAdapter
 import org.testng.Assert
 import org.testng.annotations.Test
 
-@Test(groups = arrayOf("operations")) class SayOperationsTest : BaseTest() {
+@Test(groups = arrayOf("operations"))
+class SayOperationsTest : BaseTest() {
     @Inject
     private lateinit var operation: SayOperation
 
@@ -16,9 +18,17 @@ import org.testng.annotations.Test
     }
 
     fun testOtherChannelSay() {
-        var response=operation.handleMessage(privateMessage("~say ${TEST_CHANNEL.name} hi there"))
+        var response = operation.handleMessage(privateMessage("~say ${TEST_CHANNEL.name} hi there"))
         Assert.assertEquals(response.size, 1)
         Assert.assertEquals(response[0].value, "hi there")
         Assert.assertEquals(response[0].channel!!.name, TEST_CHANNEL.name)
     }
+
+    @Test
+    fun testSayNotOnChannel() {
+        val response = operation.handleMessage(privateMessage("~say ${TEST_CHANNEL.name}a this is a test"))
+        Assert.assertEquals(response.size, 0)
+    }
+
+    // TODO add a test for say that is for a channel for which user is not an op
 }
