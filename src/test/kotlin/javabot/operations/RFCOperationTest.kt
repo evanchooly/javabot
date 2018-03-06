e6c4ccc7191550e8abba0785eb99639266a496f3
Jason Whaley <jasonwhaley@gmail.com>
2018-03-06 06:21:05 -0500

    Refactor RFCOperationTest to use @DataProvider

--- src/test/kotlin/javabot/operations/RFCOperationTest.kt
+++ src/test/kotlin/javabot/operations/RFCOperationTest.kt
@@ -3,6 +3,7 @@ package javabot.operations
 import com.antwerkz.sofia.Sofia
 import javabot.BaseTest
 import org.testng.Assert
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 import javax.inject.Inject
 
@@ -10,38 +11,18 @@ import javax.inject.Inject
     @Inject
     private lateinit var operation: RFCOperation
 
-    @Test
-    fun testBadRFCNumber() {
-        val response = operation.handleMessage(message("~rfc abd"))
-        Assert.assertEquals(response[0].value, Sofia.rfcInvalid("abd"))
-    }
-
-    @Test
-    fun testMissingRFCNumber() {
-        Assert.assertEquals(operation.handleMessage(message("~rfc "))[0].value, Sofia.rfcMissing())
-        Assert.assertEquals(operation.handleMessage(message("~rfc"))[0].value, Sofia.rfcMissing())
-    }
-
-    fun matches(r:String, m:List<String>):Boolean {
-        return m.contains(r)
-    }
-
-    @Test
-    fun testHTTPRFC() {
-        val possibleResponses = listOf(
-                Sofia.rfcSucceed("http://www.rfc-base.org/rfc-2616.html", "RFC 2616 - Hypertext Transfer Protocol HTTP/1.1"),
-                Sofia.rfcSucceed("https://tools.ietf.org/html/rfc2616", "RFC 2616 - Hypertext Transfer Protocol -- HTTP/1.1")
+    @DataProvider(name = "rfcMessages")
+    fun getRfcsCommandAndExpectedMessages(): Array<Array<*>> {
+        return arrayOf(
+                arrayOf("~rfc abd", Sofia.rfcInvalid("abd")),
+                arrayOf("~rfc", Sofia.rfcMissing()),
+                arrayOf("~rfc 2616", Sofia.rfcSucceed("https://tools.ietf.org/html/rfc2616", "RFC 2616 - Hypertext Transfer Protocol -- HTTP/1.1")),
+                arrayOf("~rfc 8675309", Sofia.rfcFail("8675309"))
         )
-        var response = operation.handleMessage(message("~rfc 2616"))
-        Assert.assertTrue(matches(response[0].value,possibleResponses), "$response")
-        response = operation.handleMessage(message("~rfc 2616"))
-        Assert.assertTrue(matches(response[0].value,possibleResponses))
     }
 
-    fun testNonexistentRFC() {
-        // sofia: rfc.fail
-        val response = operation.handleMessage(message("~rfc 2616132"))
-        Assert.assertEquals(response[0].value, Sofia.rfcFail("2616132"))
+    @Test(dataProvider = "rfcMessages")
+    fun testRfcMessage(text: String, responseValue: String)  {
+        Assert.assertEquals(operation.handleMessage(message(text))[0].value, responseValue)
     }
-
 }
