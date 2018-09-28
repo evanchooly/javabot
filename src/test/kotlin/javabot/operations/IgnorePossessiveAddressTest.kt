bca1459bef5fd40725498f70e35835b8b9ffa448
Joseph Ottinger <joeo@enigmastation.com>
2018-09-28 14:27:26 -0400

    Adding test to ignore unhandled message response for possessive refer… (#211)
    * Adding test to ignore unhandled message response for possessive reference to bot
    
    * Update .gitignore to add a final EOL
    
    * Adding final EOLN
    
    * Removing unnecessary property for test

---
+++ src/test/kotlin/javabot/operations/IgnorePossessiveAddressTest.kt
@@ -0,0 +1,34 @@
+package javabot.operations
+
+import com.antwerkz.sofia.Sofia
+import com.google.inject.Inject
+import javabot.BaseTest
+import javabot.Javabot
+import javabot.MockIrcAdapter
+import org.testng.Assert.assertEquals
+import org.testng.Assert.assertNotNull
+import org.testng.annotations.Test
+
+class IgnorePossessiveAddressTest
+@Inject constructor(val ircAdapter: MockIrcAdapter, val javabot: Javabot) : BaseTest() {
+    @Test
+    fun testConfig() {
+        assertNotNull(ircAdapter)
+    }
+
+    @Test
+    fun testNonPossessiveAddress() {
+        javabot.processMessage(message("~${javabot.nick} test"))
+        assertEquals(ircAdapter.messages.messages.size, 1)
+        assertEquals(ircAdapter.messages.messages[0], Sofia.unhandledMessage(TEST_USER))
+        ircAdapter.messages.clear()
+
+    }
+
+    @Test
+    fun testPossessiveAddress() {
+        // this message should be ignored
+        javabot.processMessage(message("~${javabot.nick}'s test"))
+        assertEquals(ircAdapter.messages.messages.size, 0)
+    }
+}
