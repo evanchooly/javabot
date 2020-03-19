14a7401b8af6e31aa50cdaaf2cbec5401562d739
Joseph Ottinger <joeo@enigmastation.com>
2020-03-18 22:29:47 -0400

    Add request-level timeouts for HttpService (#271)
    * Add request-level timeouts for HttpService
    Add browse timeouts to 45 seconds
    
    Closes #270
    
    * Updating to use named parameters since it looks nicer
    
    * Fixing extra object allocation in HttpService, correcting BrowseOperation
    to pass in the right parameter name.
    
    * Fixing return to be more idiomatic

--- src/main/kotlin/javabot/operations/BrowseOperation.kt
+++ src/main/kotlin/javabot/operations/BrowseOperation.kt
@@ -10,8 +10,10 @@ import javabot.JavabotConfig
 import javabot.Message
 import javabot.dao.AdminDao
 import javabot.operations.browse.BrowseResult
+import javabot.service.HTTP_OPTIONS
 import javabot.service.HttpService
 import javabot.service.UrlCacheService
+import java.time.Duration
 
 class BrowseOperation @Inject constructor(bot: Javabot, adminDao: AdminDao,
                                           val httpService: HttpService,
@@ -31,13 +33,17 @@ class BrowseOperation @Inject constructor(bot: Javabot, adminDao: AdminDao,
 
     private fun callService(clazz: String, module: String? = null): String {
         val sourceData = httpService.get(
-                "https://java-browser.yawk.at/api/javabotSearch/v1",
-                mapOf(
+                url = "https://java-browser.yawk.at/api/javabotSearch/v1",
+                params = mapOf(
                         "term" to clazz,
                         "artifact" to module
                 )
                         .mapNotNull { it.value?.let { value -> it.key to value } }
-                        .toMap()
+                        .toMap(),
+                options = mapOf(
+                        HTTP_OPTIONS.READ_TIMEOUT to Duration.ofSeconds(45),
+                        HTTP_OPTIONS.CALL_TIMEOUT to Duration.ofSeconds(45)
+                )
         )
         val data = ObjectMapper().configure(JsonParser.Feature.ALLOW_COMMENTS, true)
                 .readValue(sourceData, typeReference) as Array<BrowseResult>
