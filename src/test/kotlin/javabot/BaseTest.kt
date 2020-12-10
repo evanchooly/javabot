67a772994efa23a3a519a21ac8b99d5393bc75e0
evanchooly <jlee@antwerkz.com>
2020-12-10 10:05:48 -0500

    bump to the new release

--- src/test/kotlin/javabot/BaseTest.kt
+++ src/test/kotlin/javabot/BaseTest.kt
@@ -55,9 +55,11 @@ open class BaseTest {
     val testIrcChannel: MockIrcChannel by lazy {
         MockIrcChannel(ircBot.get(), TEST_CHANNEL.name)
     }
+
     val testIrcUser: MockIrcUser by lazy {
         MockIrcUser(ircBot.get(), TEST_USER.nick)
     }
+
     val testIrcHostmask: MockUserHostmask by lazy {
         MockUserHostmask(ircBot.get(), TEST_USER.nick)
     }
