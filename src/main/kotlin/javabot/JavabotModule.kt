68ef8aa10c7090f768fa7db75ef76421d10840ee
evanchooly <jlee@antwerkz.com>
2020-12-10 20:20:46 -0500

    use bottlerocket for tests

--- src/main/kotlin/javabot/JavabotModule.kt
+++ src/main/kotlin/javabot/JavabotModule.kt
@@ -5,9 +5,11 @@ import com.google.inject.AbstractModule
 import com.google.inject.Provider
 import com.google.inject.Provides
 import com.google.inject.assistedinject.FactoryModuleBuilder
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
 import dev.morphia.Datastore
 import dev.morphia.Morphia
-import dev.morphia.mapping.MapperOptions
 import javabot.dao.ChannelDao
 import javabot.dao.ConfigDao
 import javabot.model.Factoid
@@ -23,32 +25,37 @@ import java.util.HashMap
 import javax.inject.Singleton
 
 open class JavabotModule : AbstractModule() {
+    open val mongoClient: MongoClient by lazy {
+        MongoClients.create(MongoClientSettings.builder().build())
+    }
 
     private var config: JavabotConfig? = null
-
     lateinit var ircAdapterProvider: Provider<out IrcAdapter>
     lateinit var channelDaoProvider: Provider<ChannelDao>
     lateinit var configDaoProvider: Provider<ConfigDao>
-
     override fun configure() {
         configDaoProvider = binder().getProvider(ConfigDao::class.java)
         channelDaoProvider = binder().getProvider(ChannelDao::class.java)
         ircAdapterProvider = binder().getProvider(IrcAdapter::class.java)
-        install(FactoryModuleBuilder()
-                .build(ViewFactory::class.java))
+        install(
+            FactoryModuleBuilder()
+                .build(ViewFactory::class.java)
+        )
+    }
+
+    open fun client(): MongoClient? {
+        return mongoClient
     }
 
     @Provides
     @Singleton
     fun datastore(): Datastore {
         val databaseName: String = javabotConfig().databaseName()
-        val datastore = Morphia.createDatastore(databaseName, MapperOptions.DEFAULT)
+        val datastore = Morphia.createDatastore(client(), databaseName)
 
         datastore.mapper.mapPackageFromClass(JavadocClass::class.java)
         datastore.mapper.mapPackageFromClass(Factoid::class.java)
         datastore.ensureIndexes()
-
-
 
         return datastore
     }
@@ -59,13 +66,13 @@ open class JavabotModule : AbstractModule() {
         val config = configDaoProvider.get().get()
         val nick = getBotNick()
         val builder = Builder()
-                .setName(nick)
-                .setLogin(nick)
-                .setAutoNickChange(false)
-                .setCapEnabled(false)
-                .addListener(getBotListener())
-                .addServer(config.server, config.port)
-                .addCapHandler(SASLCapHandler(nick, config.password))
+            .setName(nick)
+            .setLogin(nick)
+            .setAutoNickChange(false)
+            .setCapEnabled(false)
+            .addListener(getBotListener())
+            .addServer(config.server, config.port)
+            .addCapHandler(SASLCapHandler(nick, config.password))
 
         return buildBot(builder)
     }
@@ -89,7 +96,6 @@ open class JavabotModule : AbstractModule() {
     }
 
     protected open fun loadConfigProperties(): HashMap<Any, Any> = HashMap()
-
     protected fun validate(config: JavabotConfig): JavabotConfig {
         @Suppress("UNCHECKED_CAST")
         val configClass = config.javaClass.interfaces[0] as Class<JavabotConfig>
@@ -99,13 +105,14 @@ open class JavabotModule : AbstractModule() {
             try {
                 val annotation = method.getDeclaredAnnotation(Key::class.java)
                 if (annotation != null && method.parameterCount == 0 && method.returnType != Void::class.java && method.invoke(
-                        config) == null) {
+                        config
+                    ) == null
+                ) {
                     missingKeys.add(annotation.value)
                 }
             } catch (e: ReflectiveOperationException) {
                 throw RuntimeException(e.message, e)
             }
-
         }
         if (!missingKeys.isEmpty()) {
             throw RuntimeException(Sofia.configurationMissingProperties(missingKeys))
