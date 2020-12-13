e58b464de335c128ffc0e1129e7e3cee53a8c1d3
evanchooly <jlee@antwerkz.com>
2020-12-13 10:49:42 -0500

    use the new manifest entry key

--- src/main/kotlin/javabot/javadoc/JavadocType.kt
+++ src/main/kotlin/javabot/javadoc/JavadocType.kt
@@ -4,22 +4,19 @@ import java.io.File
 import java.util.jar.Manifest
 
 enum class JavadocType {
-     JAVA6 {
-     },
-     JAVA7 {
-     },
-     JAVA8 {
-     },
-     JAVA11 {
-     };
+     JAVA6,
+     JAVA7,
+     JAVA8,
+     JAVA11;
 
-     companion object {
+    companion object {
          fun discover(root: File) : JavadocType {
              val manFile = File(root, "META-INF/MANIFEST.MF")
              if(manFile.exists()) {
                  val manifest = Manifest()
                  manifest.read(manFile.inputStream())
-                 val jdkVersion = manifest.mainAttributes.getValue("Build-Jdk")
+                 val jdkVersion = manifest.mainAttributes.getValue("Build-Jdk") ?:
+                    manifest.mainAttributes.getValue("Build-Jdk-Spec")
                  return when {
                      jdkVersion.startsWith("1.6") -> JAVA6
                      jdkVersion.startsWith("1.7") -> JAVA7
