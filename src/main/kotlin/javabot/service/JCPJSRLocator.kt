eb5a44b60457565d1f8ce87e9c5c83d20cf655ba
evanchooly <jlee@antwerkz.com>
2022-11-29 00:05:13 -0500

    fix tests bump bottlerocker

--- src/main/kotlin/javabot/service/JCPJSRLocator.kt
+++ src/main/kotlin/javabot/service/JCPJSRLocator.kt
@@ -1,39 +1,33 @@
 package javabot.service
 
-import com.google.common.base.Strings
 import com.google.inject.Inject
 import com.google.inject.Singleton
+import java.io.IOException
 import org.jsoup.Jsoup
-import java.util.*
 
 @Singleton
 class JCPJSRLocator @Inject constructor(private val httpService: HttpService) {
-    private fun locate(inputs: Map<String, String>): Map<String, String> {
-        val retVal = HashMap<String, String>()
-        val urlString = "http://www.jcp.org/en/jsr/detail?id=" + inputs["jsr"]
-        retVal.put("url", urlString)
+    private fun locate(jsr: Int): Pair<String, String?> {
+        var title: String? = null
+        val urlString = "http://www.jcp.org/en/jsr/detail?id=$jsr"
         try {
-            val data = httpService.get(urlString)
-            val doc = Jsoup.parse(data)
-            val title = doc.select("div.header1").first()
-            retVal.put("title",
-                    title
-                            .textNodes()
-                            .map { element -> element.text().trim() }
-                            .joinToString(separator = " "))
+            title = Jsoup.parse(httpService.get(urlString))
+                .select("div.header1")
+                .first()
+                ?.textNodes()
+                ?.map { element -> element.text().trim() }
+                ?.joinToString(separator = " ") ?: throw IOException()
         } catch (ignored: Exception) {
         }
 
-        return retVal
+        return urlString to title
     }
 
     fun findInformation(jsr: Int): String {
-        val inputs = HashMap<String, String>()
-        inputs.put("jsr", Integer.toString(jsr))
-        val outputs = locate(inputs)
-        if (Strings.isNullOrEmpty(outputs["title"])) {
+        val (url, title) = locate(jsr)
+        if (title.isNullOrEmpty()) {
             return ""
         }
-        return "'" + outputs["title"] + "' can be found at " + outputs["url"]
+        return "'$title' can be found at $url"
     }
 }
