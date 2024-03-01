dc8df0e7c27cc36a78747afa44b39bc1d31e9fa0
Joseph Ottinger <joeo@enigmastation.com>
2024-03-01 07:05:59 -0500

    Migrate to kGPT, use system directives in GPT prompt

--- src/main/kotlin/javabot/dao/ChatGPTDao.kt
+++ src/main/kotlin/javabot/dao/ChatGPTDao.kt
@@ -1,5 +1,9 @@
 package javabot.dao
 
+import com.enigmastation.kgpt.GPT
+import com.enigmastation.kgpt.model.BaseGPTResponse
+import com.enigmastation.kgpt.model.GPTMessage
+import com.enigmastation.kgpt.model.GPTResponse
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -12,79 +16,27 @@ import javabot.operations.throttle.BotRateLimiter
 import javabot.service.HttpService
 import java.util.concurrent.TimeUnit
 
-data class GPTMessageContainer(
-    val messages: List<GPTMessage>,
-    val model: String = "gpt-4",
-    val temperature: Double = 0.7
-)
-
-data class GPTMessage(val content: String, val role: String = "user")
-
-class GPTResponse(
-    @JsonProperty("id") val id: String,
-    @JsonProperty("object") val completion: String,
-    @JsonProperty("created") val created: Long,
-    @JsonProperty("model") val model: String,
-    @JsonProperty("choices") val choices: List<GPTChoice>,
-    @JsonProperty("usage") val usage: GPTUsage
-)
-
-class GPTUsage(
-    @JsonProperty("prompt_tokens") val promptTokens: Int,
-    @JsonProperty("completion_tokens") val completionTokens: Int,
-    @JsonProperty("total_tokens") val totalTokens: Int
-)
-
-class GPTChoice(
-    @JsonProperty("message") val message: GPTChoiceMessage,
-    @JsonProperty("index") val index: Int,
-    @JsonProperty("finish_reason") val finishReason: String
-)
-
-data class GPTChoiceMessage(
-    @JsonProperty("content") val content: String,
-    @JsonProperty("role") val role: String
-)
-
 @Singleton
 class ChatGPTDao
 @Inject
 constructor(
     private val javabotConfig: JavabotConfig,
-    private val httpService: HttpService,
 ) {
-    private val mapper: ObjectMapper
+    private val gpt= GPT(javabotConfig.chatGptKey())
 
     private val limiter: BotRateLimiter =
         BotRateLimiter(javabotConfig.chatGptLimit(), Duration.days(1).toMilliseconds())
     private val queryCache = CacheBuilder.newBuilder()
         .maximumSize(100)
         .expireAfterWrite(1, TimeUnit.DAYS)
-        .build<String, GPTResponse>();
+        .build<String, BaseGPTResponse>();
 
-    init {
-        mapper = ObjectMapper()
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-    }
+    private fun getGPTResponse(prompts:List<GPTMessage>): BaseGPTResponse = gpt.query(prompts)
 
-    private fun getGPTResponse(prompt: String): GPTResponse {
-        val data = httpService.post(
-            "https://api.openai.com/v1/chat/completions",
-            emptyMap(),
-            mapOf("Authorization" to "Bearer ${javabotConfig.chatGptKey()}"),
-            emptyMap(),
-            GPTMessageContainer(listOf(GPTMessage(prompt)))
-        )
-        val response = mapper.readValue(data, GPTResponse::class.java)
-        return response
-    }
-
-    fun sendPromptToChatGPT(key: String, prompt: String): String? {
-        val mapper = ObjectMapper()
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        return if (javabotConfig.chatGptKey().isNotEmpty() && limiter.tryAcquire()) {
-            val response = queryCache.get(key) { getGPTResponse(prompt) }
-            return response.choices.first().message.content
+    fun sendPromptToChatGPT(key: String, prompts:List<GPTMessage>): String? {
+        return if (limiter.tryAcquire()) {
+            val response = queryCache.get(key) { getGPTResponse(prompts) }
+            return response.first()
         } else {
             // no chatGPT key? No chatGPT attempt.
             null
